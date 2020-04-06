939a599464945c43c7bfba82ecc3908dc2ce1a38
lucetre <lucetre@naver.com>
2020-04-06 17:40:23 +0900

    Implement cpd for scattered videos

--- sample_video/test.m
+++ sample_video/test.m
@@ -1,72 +1,48 @@
-% scp lucetre@gogh.snu.ac.kr:/usr0/DATA/TENSOR/sample-video/video.tensor .
-addpath('tensorlab_2016-03-28');
-warning('off','all');
+addpath('../tensorlab_2016-03-28');
+warning('off', 'all');
 
-% 375 * 240 * 320 * 3
-% 86,400,000
+
+% 205 * 180(31~210) * 320 * 3
 
 currentPath = fileparts(mfilename('fullpath'));
-tensorFile = fopen(strcat(currentPath, '/video.tensor'), 'r');
+videoTensor = [];
+R = 5;
+dims = [205 180 320 3];
+numOfFrames = dims(1);
+iterFrame = 5;
+% N = numOfFrames / iterFrame;
+N = 1;
+videoTensor = NaN(dims);
 
-tic;
-% Origin Tensor
-originX = fscanf(tensorFile, '%d %d %d %d %f', [5, inf]);
-valOriginX = (originX(end, :));
-subOriginX = (originX(1:4, :))';
-dimOriginX = max(subOriginX)
-fclose(tensorFile);
-toc;
+for i = 1:N
+    tensorFile = fopen(strcat(currentPath, '/video', num2str(i-1), '.tensor'), 'r');
+    disp(i);
+    tic;
 
-originTensor = struct;
-originTensor.val = valOriginX;
-originTensor.sub = subOriginX;
-originTensor.size = dimOriginX
+    X = fscanf(tensorFile, "%d %d %d %d %d", [5, inf]);
+    for row = X
+        if row(2) > 30 & row(2) < 210
+            videoTensor(row(1), row(2)-30, row(3), row(4)) = row(5);
+        end
+    end
+    fclose(tensorFile);
 
-whos originTensor
-toc;
+    for j = 1:iterFrame
+    % for j = 1:1
+        frame = (i-1)*iterFrame + j;
+        T = videoTensor(1:frame, :, :, :);
+        whos T
 
-disp('well done');
+        options.Display = false;
+        options.Algorithm = @cpd_als;
+        options.Refinement = false;
+        % options.TolX = 1e-12;
+        Uest = cpd(T, R, options);
+        whos Uest
+        Test = cpdgen(Uest);
 
+        imwrite(uint8(squeeze(T(frame, :, :, :))), strcat('./video_frame/video_frame', num2str(frame), '_org.jpg'));
+        imwrite(uint8(squeeze(Test(frame, :, :, :))), strcat('./video_frame/video_frame', num2str(frame), '_est.jpg'));
 
-
-% R = 4;
-% U = rand(dims(1),R);
-% V = rand(dims(2),R);
-% W = rand(dims(3),R);
-% T = cpdgen({U,V,W});
-% originT = T;
-% T(randperm(numel(T),round(0.1*numel(T)))) = NaN;
-% trainT = T;
-% T = fmt(T);
-
-% model = struct;
-% model.variables.u = randn(dims(1), R);
-% model.variables.v = randn(dims(2), R);
-% model.variables.w = randn(dims(3), R);
-% % model.factors.U = {'u', @struct_nonneg};
-% % model.factors.V = {'v', @struct_nonneg};
-% % model.factors.W = {'w', @struct_nonneg};
-% model.factorizations.ntf.data = T;
-% model.factorizations.ntf.cpd = {'U','V','W'};
-% sdf_check(model, 'print')
-% tic;
-% sol = sdf_nls(model, 'Display', 10, 'MaxIter', 100, 'TolX', 1e-5)
-% toc;
-% U = sol.factors.U;
-% V = sol.factors.V;
-% W = sol.factors.W;
-% indSet = []
-% for i = 1:6
-%     for j = 1:4
-%         x = sub2ind(dims,i,j,1);
-%         indSet(end+1) = x;
-%     end
-% end
-% T = cpdgen({U, V, W}, indSet)
-% T = cpdgen({U, V, W}, [(1:6)', (7:12)', (13:18)', (19:24)'])
-% T = ful({U, V, W},':',':',1)
-% T = cpdgen({U, V, W});
-% temp = full(T);
-% originT(:,:,1)
-% trainT(:,:,1)
-% temp(:,:,1)
+    end
+end
