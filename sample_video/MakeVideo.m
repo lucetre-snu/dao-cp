12db2a27f36bfd0be708cf4b99a3b9f8b5ab521e
LUCETRE\lucetre <lucetre@github.com>
2020-04-07 12:21:06 +0900

    create video with cpd_als

---
+++ sample_video/MakeVideo.m
@@ -0,0 +1,14 @@
+
+R = 10;
+
+% outputVideoName = strcat('video_est',num2str(R),'.mp4');
+outputVideoName = strcat('video_org.mp4');
+outputVideo = VideoWriter(outputVideoName,'MPEG-4');
+outputVideo.FrameRate = 30;
+
+open(outputVideo);
+for i = 1:205
+   img = imread(strcat('video_frame/video_frame',num2str(i),'_org.jpg'));
+   writeVideo(outputVideo,img);
+end
+close(outputVideo);
